cf672f8d0ad1330f28e26372bc1f49cb89a40bc8
ilaydagnr <iguener@hm.edu>
2023-07-02 17:03:56 +0200

    Aktualisiert

--- myapp/types.ts
+++
@@ -1,9 +0,0 @@
-// types.ts
-export type RootStackParamList = {
-    Home: undefined;
-    'Monster Hunter': undefined;
-    'Istanbul 101': undefined;
-    'Tiktok Love': undefined;
-    "Eminem's M&M": undefined;
-  };
-  
--- myapp/wordContext.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const wordContext = React.createContext({
-  words: {},
-  setWords: (words: any) => {},
-});
